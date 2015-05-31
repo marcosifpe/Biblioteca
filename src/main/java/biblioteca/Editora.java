61bf73cae64b3961917aca39bbadcf8937e29987
marcosifpe <marcosifpe@gmail.com>
2015-05-31 13:10:10 -0300

    teste...

--- src/main/java/biblioteca/Editora.java
+++ src/main/java/biblioteca/Editora.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.TableGenerator;
 import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotBlank;
 
