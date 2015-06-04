0d7b3cd64eff3d503ecebccb325490a0ad6f1f0f
marcosifpe <marcosifpe@gmail.com>
2015-06-04 00:07:31 -0300

    Versão com alterações no converter.

--- src/main/java/biblioteca/Livro.java
+++ src/main/java/biblioteca/Livro.java
@@ -14,7 +14,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
