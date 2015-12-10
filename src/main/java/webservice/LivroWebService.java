2aeadf09866bf5b0234f7a6ab48d9e1b2dd3d5ea
marcosifpe <marcosifpe@gmail.com>
2015-12-10 14:52:42 -0300

    Atualização.

--- src/main/java/webservice/LivroWebService.java
+++ src/main/java/webservice/LivroWebService.java
@@ -6,8 +6,6 @@
 package webservice;
 
 import biblioteca.Livro;
-import com.google.gson.Gson;
-import java.io.StringReader;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
@@ -15,13 +13,10 @@ import javax.ejb.TransactionAttributeType;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.interceptor.Interceptors;
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonReader;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -41,21 +36,12 @@ public class LivroWebService {
     @EJB
     private LivroService livroService;
 
-    @POST
-    @Path("isbn")
+    @GET
+    @Path("isbn/{isbn}")
     @Produces("application/json")
-    @Consumes("application/json")
-    public String getLivro(String jsonIsbn, @Context HttpServletRequest request,
+    public String getLivro(@PathParam("isbn") String isbn, @Context HttpServletRequest request,
             @Context HttpHeaders httpHeaders) {
-        String isbn = getIsbn(jsonIsbn);
         Livro livro = livroService.getLivro(isbn);
         return livro.toJson();
-    }
-
-    private String getIsbn(String jsonIsbn) {
-        JsonReader jsonReader = Json.createReader(new StringReader(jsonIsbn));
-        JsonObject jsonObject = jsonReader.readObject();
-        jsonReader.close();
-        return jsonObject.getString("isbn");
     }
 }
